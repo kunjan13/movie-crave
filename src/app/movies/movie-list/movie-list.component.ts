2ad912bee1dcf7b528d2c431c073f04f07ec2295
Kunjan Patel <kunjan.patel@gatewaytechnolabs.com>
2017-03-29 18:49:00 +0530

    added movie service

--- src/app/movies/movie-list/movie-list.component.ts
+++ src/app/movies/movie-list/movie-list.component.ts
@@ -1,21 +1,22 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Movie } from '../movie';
 import { Moviecast } from '../../shared/moviecast';
+import { MovieService } from "app/movies/movie.service";
+
 @Component({
   selector: 'movcrav-movie-list',
   templateUrl: './movie-list.component.html',
-  styleUrls: ['./movie-list.component.css']
+  styleUrls: ['./movie-list.component.css'],
+  providers: [MovieService]
 })
 export class MovieListComponent implements OnInit {
-  movies: Movie[] = [
-    new Movie('Logan', "In the near future, a weary Logan cares for an ailing Professor X somewhere on the Mexican border. However, Logan's attempts to hide from the world and his legacy are upended when a young mutant arrives, pursued by dark forces.", 'https://images-na.ssl-images-amazon.com/images/M/MV5BMjI1MjkzMjczMV5BMl5BanBnXkFtZTgwNDk4NjYyMTI@._V1_UX182_CR0,0,182,268_AL__QL50.jpg', [new Moviecast("Hugh Jackman", "Logan"), new Moviecast("Dafne Keen", "Laura")]),
-    new Movie('Life', "A team of scientists aboard the International Space Station discover a rapidly evolving life form, that caused extinction on Mars, and now threatens the crew and all life on Earth.", 'https://images-na.ssl-images-amazon.com/images/M/MV5BMzAwMmQxNTctYjVmYi00MDdlLWEzMWUtOTE5NTRiNDhhNjI2L2ltYWdlXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_UX182_CR0,0,182,268_AL__QL50.jpg', [new Moviecast("Ryan Reynolds", "Rory Adams"), new Moviecast("Jake Gyllenhaal", "David Jordan")])
-  ];
+  movies: Movie[] = this.movieService.getMovies();
   @Output() movieSelected = new EventEmitter<Movie>();
   //movie = new Movie('Dummy', 'Dummy Description', 'https://pbs.twimg.com/profile_images/747456419683774464/wnl4EjM-.jpg', "");
-  constructor() { }
+  constructor(private movieService: MovieService) { }
 
   ngOnInit() {
+    //this.movies = this.movieService.getMovies();
   }
 
   OnSelected(movie: Movie){
---
+++ src/app/movies/movie.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Movie } from "app/movies/movie";
+import { Moviecast } from "app/shared/moviecast";
+
+@Injectable()
+export class MovieService {
+
+  getMovies(){
+    return [
+      new Movie('Logan', "In the near future, a weary Logan cares for an ailing Professor X somewhere on the Mexican border. However, Logan's attempts to hide from the world and his legacy are upended when a young mutant arrives, pursued by dark forces.", 'https://images-na.ssl-images-amazon.com/images/M/MV5BMjI1MjkzMjczMV5BMl5BanBnXkFtZTgwNDk4NjYyMTI@._V1_UX182_CR0,0,182,268_AL__QL50.jpg', [new Moviecast("Hugh Jackman", "Logan"), new Moviecast("Dafne Keen", "Laura")]),
+      new Movie('Life', "A team of scientists aboard the International Space Station discover a rapidly evolving life form, that caused extinction on Mars, and now threatens the crew and all life on Earth.", 'https://images-na.ssl-images-amazon.com/images/M/MV5BMzAwMmQxNTctYjVmYi00MDdlLWEzMWUtOTE5NTRiNDhhNjI2L2ltYWdlXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_UX182_CR0,0,182,268_AL__QL50.jpg', [new Moviecast("Ryan Reynolds", "Rory Adams"), new Moviecast("Jake Gyllenhaal", "David Jordan")])
+    ];
+  }
+  constructor() { }
+
+}
